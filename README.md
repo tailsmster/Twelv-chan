87595d001d5a717f1c5acd2d7ecd834cde1a3f6e
tailsmster <lotailsmonsieur62c900@gmail.com>
2024-01-03 19:45:59 -0500

    Updated README

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Twelv-Chan
-<h3> multipurpose discord bot for my servers </h3>
+!["Twelv-chan"](https://cdn.discordapp.com/attachments/1192242595594784879/1192267146747707402/Twelv-chan_Banner.png?ex=65a8744f&is=6595ff4f&hm=f90186a9d9702d69df14cb473b12fd02b9e066502f10663a5064e6f19dd76d61&)
+
+<h3> multipurpose bot for my discord servers </h3>
 
 Current goals:
 
--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-// Require the necessary discord.js classes
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-
 client.commands = new Collection();
-
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -19,7 +15,6 @@ for (const folder of commandFolders) {
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
 		} else {
@@ -28,16 +23,12 @@ for (const folder of commandFolders) {
 	}
 }
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
-
 	const command = interaction.client.commands.get(interaction.commandName);
 
 	if (!command) {
@@ -57,5 +48,4 @@ client.on(Events.InteractionCreate, async interaction => {
 	}
 });
 
-// Log in to Discord with your client's token
 client.login(token);
